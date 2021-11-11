1ed3c1a26bad897d7f39ed515be1c1f6f441c306
mellabanca <91677889+mellabanca@users.noreply.github.com>
2021-11-11 14:25:53 -0300

    Arquivos 11-11

--- sketch.js
+++ sketch.js
@@ -1,33 +1,34 @@
 var trex, trexCorrendo;
-
+var chao, chaofotinha;
 function preload(){
-
-  trexCorrendo = loadAnimation("trex1.png", "trex3.png", "trex4.png");
-
+trexCorrendo = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+chaofotinha = loadImage("ground2.png");
 }
-
 function setup(){
-  createCanvas(600,200)
-  
-  trex = createSprite(50,160,20,50);
-  trex.addAnimation("correndo", trexCorrendo);
-  trex.scale = 0.5;
-  borda = createEdgeSprites();
- 
+createCanvas(600,200);
+trex = createSprite(50,160,20,50);
+trex.addAnimation("correndo", trexCorrendo);
+trex.scale = 0.5;
+borda = createEdgeSprites();
+chao = createSprite(200, 180, 400, 20);
+chao.addImage ("chao", chaofotinha);
+chao.x = chao.width/2;
 }
-
 function draw(){
-  //cria um fundo branco
-  background("white")
-  //fazendo o trex pular
-  if(keyDown("space")){
-    trex.velocityY = -10;
-  }
-  //sistema de gravidade
-  trex.velocityY = trex.velocityY + 1;
-  //impede que o trex caixa
-  trex.collide(borda[3]);
-  //desenha todos os sprites
-  drawSprites();
-
+//cria um fundo branco
+background("white")
+//fazendo o trex pular
+if(keyDown("space")){
+trex.velocityY = -10;
+}
+//sistema de gravidade
+trex.velocityY = trex.velocityY + 1;
+//impede que o trex caixa
+trex.collide(chao);
+chao.velocityX = -2;
+if(chao.x < 0){
+chao.x = chao.width/2;
+}
+//desenha todos os sprites
+drawSprites();
 }
