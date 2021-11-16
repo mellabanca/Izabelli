c5234285eb95c3c77d744584ab0035cdb9b343b2
mellabanca <91677889+mellabanca@users.noreply.github.com>
2021-11-16 13:25:51 -0300

    Aula 16/11

--- sketch.js
+++ sketch.js
@@ -1,34 +1,65 @@
 var trex, trexCorrendo;
+
 var chao, chaofotinha;
+
+var chaoinvisivel;
+
 function preload(){
-trexCorrendo = loadAnimation("trex1.png", "trex3.png", "trex4.png");
-chaofotinha = loadImage("ground2.png");
+    trexCorrendo = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+    chaofotinha = loadImage("ground2.png");
+
 }
+
 function setup(){
-createCanvas(600,200);
-trex = createSprite(50,160,20,50);
-trex.addAnimation("correndo", trexCorrendo);
-trex.scale = 0.5;
-borda = createEdgeSprites();
-chao = createSprite(200, 180, 400, 20);
-chao.addImage ("chao", chaofotinha);
-chao.x = chao.width/2;
+    createCanvas(600,200);
+
+    trex = createSprite(50,160,20,50);
+    trex.addAnimation("correndo", trexCorrendo);
+    trex.scale = 0.5;
+
+    borda = createEdgeSprites();
+
+    chao = createSprite(200, 180, 400, 20);
+    chao.addImage ("chao", chaofotinha);
+    chao.x = chao.width/2;
+
+    chaoinvisivel = createSprite(200, 190, 400, 10);
+    chaoinvisivel.visible = false;
+
+    var numeroaleatorio = Math.round(random(1,100));
+    console.log(numeroaleatorio);
+
 }
+
 function draw(){
-//cria um fundo branco
-background("white")
-//fazendo o trex pular
-if(keyDown("space")){
-trex.velocityY = -10;
+
+    //cria um fundo branco
+    background("white")
+
+    //console.log(trex.y);
+
+    //fazendo o trex pular
+    if(keyDown("space") && trex.y >= 150){
+        trex.velocityY = -10;
+    }
+
+    //sistema de gravidade
+    trex.velocityY = trex.velocityY + 1;
+
+    //impede que o trex caixa
+    trex.collide(chaoinvisivel);
+    chao.velocityX = -2;
+    if(chao.x < 0){
+        chao.x = chao.width/2;
+    }
+
+    nuvenzinhasAleatorias();
+
+    //desenha todos os sprites
+    drawSprites();
+
 }
-//sistema de gravidade
-trex.velocityY = trex.velocityY + 1;
-//impede que o trex caixa
-trex.collide(chao);
-chao.velocityX = -2;
-if(chao.x < 0){
-chao.x = chao.width/2;
-}
-//desenha todos os sprites
-drawSprites();
+
+function nuvenzinhasAleatorias(){
+
 }
